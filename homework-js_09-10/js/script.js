5311a7435a792a62fa734cb2cb34ee18774ceafc
ollivver <ol.vertiy@yahoo.com>
2016-04-12 21:44:14 +0300

    added homework js_09-10

---
+++ homework-js_09-10/js/script.js
@@ -0,0 +1,207 @@
+/* call and control carousel plugin */
+$(function() {
+    $('.jcarousel')
+        .jcarousel({
+            animation: 'slow'
+        })
+        .jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        });
+
+        /* 'next-' and 'prev-' button control */
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+        });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+        });
+
+        /* pagination control */    
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+
+});
+
+/* custom selectBox  initialize */
+$(function() {
+  $("#hover_select").customselect({
+    search:     false,
+    hoveropen:  true
+  });
+});
+
+/*drop-down menu */
+$(function() {
+    /* hide menu items at startup */
+    $('.sub-menu').slideUp(100);
+
+    $('.dropdown').hover(
+        function(){
+            $(this).children('.sub-menu').stop(true, false, true).fadeIn(600);
+        },
+        function(){
+            $(this).children('.sub-menu').stop(true, false, true).fadeOut(600);
+        }
+    );
+});
+
+/*
+$(document).ready(function() {
+    $( '.dropdown' ).hover(
+        function(){
+            $(this).children('.sub-menu').stop().slideDown(200);
+        },
+        function(){
+            $(this).children('.sub-menu').stop().slideUp(200);
+        }
+    );
+});
+*/
+
+/* changing background colors for drop-down menu (jquery.color plugin) */
+$(function(){
+    $('ul.sub-menu li').mouseenter(
+      function () {
+        $(this).animate({
+            backgroundColor: "#71da71",
+        }, 500 );
+    });
+
+    $('ul.sub-menu li').mouseleave(function() {
+        $(this).animate({
+            backgroundColor: "#248f24",
+        }, 500 );
+    });
+
+});
+
+/* custom checkbox from xiper.net */
+$(function(){
+
+    $(".niceCheck").each(
+/* при загрузке страницы меняем обычные на стильные checkbox */
+    function() {
+
+        changeCheckStart($(this));
+            });
+
+});
+
+function changeCheck(el)
+/* 
+    функция смены вида и значения чекбокса при клике на контейнер чекбокса (тот, который отвечает за новый вид)
+    el - span контейнер для обычного чекбокса
+    input - чекбокс
+*/
+{
+    var el = el,
+        input = el.find("input").eq(0);
+          
+    if(el.attr("class").indexOf("niceCheckDisabled")==-1)
+    {   
+        if(!input.attr("checked")) {
+            el.addClass("niceChecked");
+            input.attr("checked", true);
+        } else {
+            el.removeClass("niceChecked");
+            input.attr("checked", false).focus();
+        }
+    }
+    
+    return true;
+}
+
+function changeVisualCheck(input)
+{
+/*
+    меняем вид чекбокса при смене значения
+*/
+var wrapInput = input.parent();
+    if(!input.attr("checked")) {
+        wrapInput.removeClass("niceChecked");
+    }
+    else
+    {
+        wrapInput.addClass("niceChecked");
+    }
+}
+
+function changeCheckStart(el)
+/* 
+    новый чекбокс выглядит так <span class="niceCheck"><input type="checkbox" name="[name check]" id="[id check]" [checked="checked"] /></span>
+    новый чекбокс получает теже name, id и другие атрибуты что и были у обычного
+*/
+{
+
+try
+{
+var el = el,
+    checkName = el.attr("name"),
+    checkId = el.attr("id"),
+    checkChecked = el.attr("checked"),
+    checkDisabled = el.attr("disabled"),
+    checkTab = el.attr("tabindex"),
+    checkValue = el.attr("value");
+    if(checkChecked)
+        el.after("<span class='niceCheck niceChecked'>"+
+            "<input type='checkbox'"+
+            "name='"+checkName+"'"+
+            "id='"+checkId+"'"+
+            "checked='"+checkChecked+"'"+
+            "value='"+checkValue+"'"+
+            "tabindex='"+checkTab+"' /></span>");
+    else
+        el.after("<span class='niceCheck'>"+
+            "<input type='checkbox'"+
+            "name='"+checkName+"'"+
+            "id='"+checkId+"'"+
+             "value='"+checkValue+"'"+
+            "tabindex='"+checkTab+"' /></span>");
+    
+    /* если checkbox disabled - добавляем соотвсмтвующи класс для нужного вида и добавляем атрибут disabled для вложенного chekcbox */      
+    if(checkDisabled)
+    {
+        el.next().addClass("niceCheckDisabled");
+        el.next().find("input").eq(0).attr("disabled","disabled");
+    }
+    
+    /* цепляем обработчики стилизированным checkbox */      
+    el.next().bind("mousedown", function(e) { changeCheck($(this)) });
+    el.next().find("input").eq(0).bind("change", function(e) { changeVisualCheck($(this)) });
+    if($.browser.msie)
+    {
+        el.next().find("input").eq(0).bind("click", function(e) { changeVisualCheck($(this)) });   
+    }
+    el.remove();
+}
+catch(e)
+{
+    // если ошибка, ничего не делаем
+}
+
+    return true;
+}
+
